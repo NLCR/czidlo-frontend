ca913b5131c5ff5a6e9361cdc999bf9364fa90f2
rzeh4n <martin.rehanek@gmail.com>
2025-09-19 13:28:04 +0200

    setup environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+    useStaticRuntimeConfig: true, // DŮLEŽITÉ: pokud je true, konfigurace se načítá z env.json; Pro produkci vždy true, pro lokální vývoj (environment.local.ts) false
+
+    // overriden with env.json if useStaticRuntimeConfig is true
+    devMode: false, // pro produkci ziskej z promenne APP_DEV_MODE (přes env.json)
+    environmentName: 'deployed (branch main)', // pro produkci ziskej z promenne APP_ENV_NAME (přes env.json)
+    environmentCode: 'd_m', // pro produkci ziskej z promenne APP_ENV_CODE (přes env.json)
+
+    czidloApiServiceBaseUrl: '', // pro produkci ziskej z promenne APP_CZIDLO_API_SERVICE_URL (přes env.json)
+        
+}
