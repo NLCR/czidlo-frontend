4df260b0ce31f9b3cce7bb7ed300fc8ac699ab3e
pavluska <pavla.rychtarova@trinera.cz>
2025-09-29 17:04:26 +0200

    pridany processes

--- src/app/body/processes/processes.component.ts
+++ src/app/body/processes/processes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-processes',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './processes.component.scss'
 })
 export class ProcessesComponent {
+
+    processes: any[] = [];
+
+    constructor(private apiService: ApiService) {}
+
+    ngOnInit() {
+        this.loadProcesses();
+    }
+    loadProcesses() {
+        this.apiService.getProcesses().subscribe({
+            next: (data) => {
+                this.processes = data;
+                console.log('Processes loaded:', this.processes);
+            },
+            error: (error) => {
+                console.error('Error loading processes:', error);
+            }
+        });
+    }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@angular/core';
 import { environment as staticEnv } from '../../environments/environment';
+import { Observable, of, throwError } from 'rxjs';
+import { map, catchError, delay, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
     private apiUrl: string;
 
-    constructor() {
+    constructor(private http: HttpClient) {
         this.apiUrl = staticEnv.czidloApiServiceBaseUrl || 'http://localhost:3000/api'; // Defaultní hodnota
         console.log('API URL:', this.apiUrl);
+    }
+
+    doGet(url: string): Observable<Object> {
+        return this.http.get(encodeURI(url)).pipe(catchError(this.handleError));
+    }
+
+    private handleError(error: Response) {
+        if (error.status === 404) {
+            return throwError(() => 'Not found');
+        } else if (error.status === 401 || error.status === 403) {
+            return throwError(() => 'Unauthorized');
+        }
+        return throwError(() => 'Server error');
+    }
+
+    // PROCESSES
+    getProcesses(): Observable<any> {
+        const url = `${this.apiUrl}/processes`;
+        return this.doGet(url);
     }
 }
