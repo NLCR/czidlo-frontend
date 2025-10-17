7c194d93e5027f53defd9a1e02f750e3950baf74
rzeh4n <martin.rehanek@gmail.com>
2025-10-17 13:24:41 +0200

    cleanup dev environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,9 +8,7 @@ export const environment = {
     environmentCode: 'd_d', // pro produkci ziskej z promenne APP_ENV_CODE (přes env.json)
 
     czidloApiServiceBaseUrl: '', // pro produkci ziskej z promenne APP_CZIDLO_API_SERVICE_URL (přes env.json)
-    czidloApiServiceInfoUrl: '', // pro produkci ziskej z promenne APP_CZIDLO_API_SERVICE_INFO_URL (přes env.json)
-    czidloApiServiceRulesUrl: '', // pro produkci ziskej z promenne APP_CZIDLO_API_SERVICE_LOG_URL (přes env.json)
-
+    
     //TODO: presunout do konfigurace
     pageInfoCzUrl: 'https://raw.githubusercontent.com/trineracz/czidlo-frontend-tmp/refs/heads/main/Informace.md',
     pageRulesCzUrl: 'https://raw.githubusercontent.com/trineracz/czidlo-frontend-tmp/refs/heads/main/Pravidla.md',
